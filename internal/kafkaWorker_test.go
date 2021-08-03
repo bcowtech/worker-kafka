9ea262af97a5fbf3a2db59946b01d4eb141869b8
Wilhelm Liao <williaml.twcp@gmail.com>
2021-08-03 21:47:17 +0800

    WIP[v0.2]: enhance test case

--- internal/kafkaWorker_test.go
+++ internal/kafkaWorker_test.go
@@ -6,9 +6,16 @@ import (
 	"os"
 	"testing"
 	"time"
+
+	kafka "github.com/bcowtech/lib-kafka"
 )
 
 func TestKafkaWorker(t *testing.T) {
+	err := setupTest()
+	if err != nil {
+		t.Fatal(err)
+	}
+
 	worker := &KafkaWorker{
 		PollingTimeout: 30 * time.Millisecond,
 		PingTimeout:    3 * time.Second,
@@ -39,4 +46,28 @@ type mockMessageHandler struct{}
 
 func (h *mockMessageHandler) ProcessMessage(worker *ConsumeWorker, message *Message) {
 	fmt.Printf("Message on %s: %s: %s\n", message.TopicPartition, string(message.Key), string(message.Value))
+}
+
+func setupTest() error {
+	p, err := kafka.NewProducer(&kafka.ProducerOption{
+		FlushTimeout: 3 * time.Second,
+		PingTimeout:  3 * time.Second,
+		ConfigMap: &kafka.ConfigMap{
+			"client.id":         "gotest",
+			"bootstrap.servers": os.Getenv("KAFKA_BOOTSTRAP_SERVERS"),
+		},
+	})
+	if err != nil {
+		return err
+	}
+
+	topic := "myTopic"
+	for _, word := range []string{"Welcome", "to", "the", "Confluent", "Kafka", "Golang", "client"} {
+		p.WriteMessage(&kafka.Message{
+			TopicPartition: kafka.TopicPartition{Topic: &topic, Partition: kafka.PartitionAny},
+			Value:          []byte(word),
+		}, nil)
+	}
+	p.Close()
+	return nil
 }
